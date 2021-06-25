acc02fdc4c3711af165ef89965adeb08986a07de
ThankfulSam <sam97.university@gmail.com>
2021-06-25 10:05:27 +0200

    created Contact Model

---
+++ models/Contact.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "contatti".
+ *
+ * @property int $id
+ * @property string $nome
+ * @property string $cognome
+ * @property string $telefono
+ * @property string|null $indirizzo
+ */
+class Contact extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'contatti';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nome', 'cognome', 'telefono'], 'required'],
+            [['nome', 'cognome'], 'string', 'max' => 30],
+            [['telefono'], 'string', 'max' => 15],
+            [['indirizzo'], 'string', 'max' => 40],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nome' => 'Nome',
+            'cognome' => 'Cognome',
+            'telefono' => 'Telefono',
+            'indirizzo' => 'Indirizzo',
+        ];
+    }
+}
